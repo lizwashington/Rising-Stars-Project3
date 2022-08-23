83b5d7cb396d2b38b4d5784d7a76c2424d54cb49
enajera1 <eriknajera013@gmail.com>
2022-08-22 19:52:15 -0500

    apollo to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import React from "react";
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
@@ -11,31 +12,51 @@ import AuthRoute from "./components/AuthRoute";
 // import { AuthProvider } from "./utils/auth";
 import Signup from "./components/Signup.js";
 
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: '/graphql', // Uniform Resource Identifier
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 //once the utils auth is set up, wrap below in <AuthProvider> so it can't be accessed unless auth
 
 function App() {
 
   return (
-    <>
-      
-      <AuthRoute>
-        <Router>
-          
-          <Nav />
-              <Routes>
-                <Route path="/signup" element={<Signup/>} />
-                <Route path="/login"  element={<Login />} />
-                <Route path="/dashboard"  element={<Dashboard />} />
-                <Route path="/" element={<Homepage />} />
-                <Route path="/Messaging" element={<Messaging />} />
-                <Route path="/MyAccount" element={<MyAccount />} />
-  
-              </Routes>
-           
-        </Router>
-      </AuthRoute>
-      
-    </>
+    
+      <ApolloProvider client={client}>
+        <>
+          <Router>
+            
+            <Nav />
+                <Routes>
+                  <Route path="/signup" element={<Signup/>} />
+                  <Route path="/login"  element={<Login />} />
+                  <Route path="/dashboard"  element={<Dashboard />} />
+                  <Route path="/" element={<Homepage />} />
+                  <Route path="/Messaging" element={<Messaging />} />
+                  <Route path="/MyAccount" element={<MyAccount />} />
+    
+                </Routes>
+            
+          </Router>
+        </>
+      </ApolloProvider>
   );
 }
 
