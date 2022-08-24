ab86674204a01968b76b68d1fa06f46f89d76929
enajera1 <eriknajera013@gmail.com>
2022-08-23 20:10:25 -0500

    auth added into nav

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,18 +1,44 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import {AuthProvider} from '../../utils/auth'
-
-
-
-
+import Auth from '../../utils/auth'
 
 const Nav = () => {
+    const logout = event => {
+      event.preventDefault();
+      Auth.logout();
+    };
+  
 
     return (
         <header className="flex-row px-1">
 
             <nav>
-
+            {Auth.loggedIn() ? (
+            <>
+                <NavLink to='/Dashboard'>
+                    Dashboard
+                </NavLink>
+                <NavLink to='/Messaging'>
+                    Messaging
+                </NavLink>
+                <NavLink to='/MyAccount'>
+                    My Account
+                </NavLink>
+                <NavLink to='/'>
+                    Homepage
+                </NavLink>
+              <NavLink to="/profile">Me</NavLink>
+              <a href="/" onClick={logout}>
+                Logout
+              </a>
+            </>
+            ) : (
+            <>
+              <NavLink to="/Login">Login</NavLink>
+              <NavLink to="/Signup">Signup</NavLink>
+            </>
+          )}
+{/* 
                 <>
                 <NavLink to='/Dashboard'>
                     Dashboard
@@ -30,7 +56,7 @@ const Nav = () => {
                 <>
                 <NavLink to="/Login">Login</NavLink>
                 <NavLink to="/Signup">Signup</NavLink>
-              </>
+              </> */}
             </nav>
         </header>
     );
