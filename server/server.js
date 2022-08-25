1c44e00a6d329e7c6e0eeb04d1016cb7000f9b79
keithmw <keithmw5597@yahoo.com>
2022-08-25 17:52:34 -0500

    heroku test

--- server/server.js
+++ server/server.js
@@ -38,7 +38,7 @@ await server.start();
 server.applyMiddleware({ app });
 
 db.once('open', () => {
-    app.listen(PORT, () => {
+    app.listen(process.env.PORT, () => {
       console.log(`API server running on port ${PORT}!`);
       // log where we can go to test our GQL API
       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
