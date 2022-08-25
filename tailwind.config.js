55cc67c3aecfe93953479332dc48aef199975ef0
keithmw <keithmw5597@yahoo.com>
2022-08-24 19:43:11 -0500

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './src/components/Nav/index.js',
+    './src/App.js',
+    './src/index.js',
+    './src/components/CommentForm/index.js',
+    './src//components/Feed/index.js',
+    './src/components/Homepage/index.js',
+    './src/components/Login/index.js',
+    './src/components/Messaging/index.js',
+    './src/components/PostForm/index.js',
+    './src/components/PostList/index.js',
+    './src/components/Signup.js/index.js',
+    './src/components/SinglePost/index.js',
+    './src/components/Users/index.js',
+    './src/pages/home.js',
+    './src/pages/login.js',
+    './src/pages/nomatch.js',
+    './src/pages/profile.js',
+    './src/pages/signup.js',
+    './src/pages/SinglePost.js'
+
+  ],
   theme: {
     extend: {},
   },
